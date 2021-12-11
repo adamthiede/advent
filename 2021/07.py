5d3b960f03a787db9be2533ada52fe063c9b2713
Adam Thiede <adamj@mailbox.org>
2021-12-11 10:44:09 -0800

    inefficient d7!

--- 2021/07.py
+++ 2021/07.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
-f=open("00.txt")
+f=open("07.txt")
 lines=f.readlines()
+arr=list(map(int,lines[0].split(",")))
+
 def part1():
     print("part1")
+    arrMin=min(arr)
+    arrMax=max(arr)
+    print(arrMin,arrMax)
+    sel=arrMin
+    totalUsed=[]
+    for i in range(arrMin,arrMax):
+        myFuel=0
+        for x in arr:
+            myFuel+=abs(x-i)
+        print(myFuel)
+        totalUsed.append(myFuel)
+    print(min(totalUsed))
 
 def part2():
     print("part2")
+    arrMin=min(arr)
+    arrMax=max(arr)
+    print(arrMin,arrMax)
+    sel=arrMin
+    totalUsed=[]
+    for i in range(arrMin,arrMax):
+        myFuel=0
+        for x in arr:
+            for a in range(1,abs(x-i)+1):
+                myFuel+=a
+            #myFuel+=abs(x-i)
+#        print(myFuel)
+        print(i)
+        totalUsed.append(myFuel)
+    print(min(totalUsed))
 
-part1()
+
+
+#part1()
 part2()
