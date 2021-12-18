2e825349c93488c38f001275240d8b166d4869d3
Elagost <me@elagost.com>
2021-12-18 13:40:51 -0800

    09 partial

---
+++ 2021/09.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+f=open("09.txt")
+lines=f.readlines()
+field=[]
+points=[]
+ct=0
+for x in lines:
+    field.append(x.strip())
+    points.append([])
+    for y in range(0,len(x.strip())):
+        points[ct].append(0)
+    ct+=1
+
+print(len(field),len(field[0]))
+print(len(points),len(points[0]))
+
+def part1():
+    totalsum=0
+    for y in range(0,len(field)):
+        for x in range(0,len(field[0])):
+            if x<len(field)-1 and y<len(field)-1 and x>0 and y>0:
+                mypoint=int(field[x][y])
+                # is in center of field
+                if mypoint < int(field[x][y+1]) and mypoint < int(field[x][y-1]) and mypoint < int(field[x+1][y]) and mypoint < int(field[x+1][y]):
+                    # is a low point
+                    risk=1+int(field[x][y])
+                    print("low point.",mypoint,"at",x,y,risk)
+                    points[x][y]=risk
+                    totalsum+=risk
+    print(totalsum)
+     
+
+def part2():
+    print("part2")
+
+part1()
+part2()
