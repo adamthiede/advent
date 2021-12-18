1ba41c7e7b5ada3ab499c72cf477e9fa425a1f01
Adam Thiede <adamj@mailbox.org>
2021-12-18 08:21:32 -0800

    2021 8 1

--- 2021/08.py
+++ 2021/08.py
@@ -11,8 +11,37 @@ for line in lines:
 
 print(a1[0]," | ",a2[0])
 
+# 7 segment display draw
+draw={
+    0:"abcefg",
+    1:"cf",
+    2:"acdeg",
+    3:"acdfg",
+    4:"bcdf",
+    5:"abdfg",
+    6:"abdefg",
+    7:"acf",
+    8:"abcdefg",
+    9:"abcdfg"
+}
+
+# how many segments are 1, 4, 7, or 8
+easy={
+    2:1,
+    4:4,
+    3:7,
+    7:8
+}
+
 def part1():
+    ct=0
     print("part1")
+    for i in a2:
+        print(ct,i)
+        for j in i:
+            if len(j) in easy:
+                ct+=1
+    print(ct)
 
 def part2():
     print("part2")
