fc4983d3a83c3bdb042c104b606d0e19497fb62a
Elagost <me@elagost.com>
2021-12-19 05:53:36 -0800

    working 2021 09 p1

--- 2021/09.py
+++ 2021/09.py
@@ -16,43 +16,42 @@ print(len(points),len(points[0]))
 
 def part1():
     totalsum=0
-    for y in range(0,len(field)-1):
-        for x in range(0,len(field[0])-1):
+    for y in range(0,len(field)):
+        for x in range(0,len(field[0])):
             mypoint=int(field[x][y])
-            if x==0 and y==len(field)-1:
-                if mypoint < int(field[x][y-1]) and mypoint < int(field[x+1][y]):
-                    risk=1+int(field[x][y])
-                    print("low point.",mypoint,"at",x,y,risk)
-                    points[x][y]=risk
-                    totalsum+=risk
-            if x==len(field)-1 and y==0:
-                if mypoint < int(field[x][y+1]) and mypoint < int(field[x-1][y]):
-                    risk=1+int(field[x][y])
-                    print("low point.",mypoint,"at",x,y,risk)
-                    points[x][y]=risk
-                    totalsum+=risk
-            if x==len(field)-1 and y==len(field)-1:
-                if mypoint < int(field[x][y-1]) and mypoint < int(field[x-1][y]):
-                    risk=1+int(field[x][y])
-                    print("low point.",mypoint,"at",x,y,risk)
-                    points[x][y]=risk
-                    totalsum+=risk
-            if x==0 and y==0:
-                if mypoint < int(field[x][y+1]) and mypoint < int(field[x+1][y]):
-                    risk=1+int(field[x][y])
-                    print("low point.",mypoint,"at",x,y,risk)
-                    points[x][y]=risk
-                    totalsum+=risk
-            if x<len(field)-1 and y<len(field)-1 and x>0 and y>0:
-                # is in center of field
-                if mypoint < int(field[x][y+1]) and mypoint < int(field[x][y-1]) and mypoint < int(field[x+1][y]) and mypoint < int(field[x-1][y]):
-                    # is a low point
-                    risk=1+int(field[x][y])
-                    print("low point.",mypoint,"at",x,y,risk)
-                    points[x][y]=risk
-                    totalsum+=risk
+
+            u=False
+            d=False
+            l=False
+            r=False
+
+            if y<1:
+                u=True
+            elif mypoint < int(field[x][y-1]):
+                u=True
+
+            if y>=len(field)-1:
+                d=True
+            elif mypoint < int(field[x][y+1]):
+                d=True
+
+            if x<1:
+                l=True
+            elif mypoint < int(field[x-1][y]):
+                l=True
+
+            if x>=len(field[0])-1:
+                r=True
+            elif mypoint < int(field[x+1][y]):
+                r=True
+
+            if u and d and l and r:
+                # is a low point
+                risk=1+int(field[x][y])
+                print("low point.",mypoint,"at",x,y,risk)
+                points[x][y]=risk
+                totalsum+=risk
     print(totalsum)
-     
 
 def part2():
     print("part2")
