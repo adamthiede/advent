a89a6ead68c28cac5e69d1e30265c32b68387566
Adam Thiede <adamj@mailbox.org>
2022-12-01 07:14:19 -0600

    2022 day 1

---
+++ 2022/01.go
@@ -0,0 +1,83 @@
+package main
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sort"
+)
+
+// slice printer from go tour
+func psliceS(s []string) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+func psliceI(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+// error checker helper function
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func p1(x []int) int{
+	max:=0
+	val:=0
+	for i:=range x {
+		if x[i]==0 {
+			fmt.Printf("Had %d cals at %d \n",val,i)
+			if val>max {
+				max=val
+			}
+			val=0
+		} else {
+			val+=x[i]
+		}
+	}
+	return max
+}
+
+func p2(x []int) int{
+	top3:=0
+	val:=0
+	amts:=make([]int,0)
+	for i:=range x {
+		if x[i]==0 {
+			fmt.Printf("Had %d cals at %d \n",val,i)
+			amts=append(amts,val)
+			val=0
+		} else {
+			val+=x[i]
+		}
+	}
+	sort.Sort(sort.IntSlice(amts))
+	psliceI(amts)
+	top3+=amts[len(amts)-1]+amts[len(amts)-2]+amts[len(amts)-3]
+	return top3
+}
+
+func main() {
+	// read whole file
+	text, err := os.ReadFile("01.txt")
+	check(err)
+
+	// split intput text on value; store in slice
+	splitter:="\n"
+	txt:=strings.Split(string(text),splitter)
+	// convert to int slice
+	num:=make([]int, len(txt))
+	for i,s:=range txt{
+		num[i], _ = strconv.Atoi(s)
+	}
+	//psliceS(txt)
+	psliceI(num)
+	//bkup:=make([]int, len(num))
+	//copy(bkup, num)
+	//psliceI(bkup)
+	z:=p1(num)
+	fmt.Printf("P1 answer: %d \n",z)
+	y:=p2(num)
+	fmt.Printf("P2 answer: %d \n",y)
+}
+
